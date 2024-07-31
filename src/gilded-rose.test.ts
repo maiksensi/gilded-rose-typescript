69d00056a7dfd142972315db9778e1dd071814c2
Alexander Backes <alexander.backes@codecentric.de>
2024-07-31 13:26:07 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/gilded-rose.test.ts

--- src/gilded-rose.test.ts
+++ src/gilded-rose.test.ts
@@ -1,6 +1,45 @@
 describe("Gilded Rose", () => {
-  it("should be just fine", () => {
-    const isFine = false;
-    expect(isFine).toBe(true);
+  describe("Normal item", () => {
+    it("should the quality of an item is never negative", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
+
+    it("should the quality of an item is never more than 50", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
+  });
+
+  describe("Legendary Items", () => {
+    it("should have quality 80", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
+
+    it("should never have to be sold", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
+  });
+
+  describe("Aged Brie Item", () => {
+    it("should do something", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
+  });
+
+  describe("Backstage passes Item", () => {
+    it("should do something", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
   });
 });
