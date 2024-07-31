814ba6659969a8d4f40eb5808d2ff485a2af18ee
Marcel Meyer <marcel@devdu.de>
2024-07-31 13:47:04 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/gilded-rose.test.ts

--- src/gilded-rose.test.ts
+++ src/gilded-rose.test.ts
@@ -6,12 +6,6 @@ describe("Gilded Rose", () => {
       // ASSERT
     });
 
-    it("should never have a quality more than 50", () => {
-      // ARRANGE
-      // ACT
-      // ASSERT
-    });
-
     it("should degrade its quality by 1 before the sell-by date", () => {
       // ARRANGE
       // ACT
@@ -23,15 +17,15 @@ describe("Gilded Rose", () => {
       // ACT
       // ASSERT
     });
-  });
 
-  describe("Sulfuras Item", () => {
-    it("should never have a negative quality", () => {
+    it("should decrease the sellIn by 1 each day", () => {
       // ARRANGE
       // ACT
       // ASSERT
     });
+  });
 
+  describe("Sulfuras Item", () => {
     it("should always have quality 80", () => {
       // ARRANGE
       // ACT
@@ -43,27 +37,9 @@ describe("Gilded Rose", () => {
       // ACT
       // ASSERT
     });
-
-    it("should degrade its quality by 1 before the sell-by date", () => {
-      // ARRANGE
-      // ACT
-      // ASSERT
-    });
-
-    it("should degrade its quality by 2 after the sell-by date", () => {
-      // ARRANGE
-      // ACT
-      // ASSERT
-    });
   });
 
   describe("Aged Brie Item", () => {
-    it("should never have a negative quality", () => {
-      // ARRANGE
-      // ACT
-      // ASSERT
-    });
-
     it("should increase in quality by 1 each day", () => {
       // ARRANGE
       // ACT
@@ -76,13 +52,7 @@ describe("Gilded Rose", () => {
       // ASSERT
     });
 
-    it("should degrade its quality by 1 before the sell-by date", () => {
-      // ARRANGE
-      // ACT
-      // ASSERT
-    });
-
-    it("should degrade its quality by 2 after the sell-by date", () => {
+    it("should decrease the sellIn by 1 each day", () => {
       // ARRANGE
       // ACT
       // ASSERT
@@ -90,13 +60,7 @@ describe("Gilded Rose", () => {
   });
 
   describe("Backstage passes Item", () => {
-    it("should never have a negative quality", () => {
-      // ARRANGE
-      // ACT
-      // ASSERT
-    });
-
-    it("should do something", () => {
+    it("should have a quality of zero after the sell-by date", () => {
       // ARRANGE
       // ACT
       // ASSERT
@@ -108,13 +72,25 @@ describe("Gilded Rose", () => {
       // ASSERT
     });
 
-    it("should degrade its quality by 1 before the sell-by date", () => {
+    it("should increase in quality by 1 each day when sellIn > 10", () => {
       // ARRANGE
       // ACT
       // ASSERT
     });
 
-    it("should degrade its quality by 2 after the sell-by date", () => {
+    it("should increase in quality by 2 each day when 5 < sellIn <= 10", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
+
+    it("should increase in quality by 3 each day when 0 < sellIn <= 5", () => {
+      // ARRANGE
+      // ACT
+      // ASSERT
+    });
+
+    it("should decrease the sellIn by 1 each day", () => {
       // ARRANGE
       // ACT
       // ASSERT
